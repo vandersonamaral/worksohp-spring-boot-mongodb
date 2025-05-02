68c98381d085020b2159104bccf6441a7483720a
vandersonamaral <vandersonamaral27@gmail.com>
2025-05-02 09:00:00 -0300

    feat: Referencing user posts

--- src/main/java/com/amaraldev/workshopmongo/domain/User.java
+++ src/main/java/com/amaraldev/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.amaraldev.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -13,6 +16,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> post = new ArrayList<>();
 
     public User() {
     }
@@ -45,6 +51,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPost() {
+        return post;
+    }
+
+    public void setPost(List<Post> post) {
+        this.post = post;
     }
 
     @Override
