08949c888aab8149e52f4d7cec91ed8e36d96040
vandersonamaral <vandersonamaral27@gmail.com>
2025-05-02 09:25:28 -0300

    feat: Getting a post by id

---
+++ src/main/java/com/amaraldev/workshopmongo/resources/PostResource.java
@@ -0,0 +1,24 @@
+package com.amaraldev.workshopmongo.resources;
+
+import com.amaraldev.workshopmongo.domain.Post;
+import com.amaraldev.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = postService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
+
---
+++ src/main/java/com/amaraldev/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.amaraldev.workshopmongo.services;
+
+import com.amaraldev.workshopmongo.domain.Post;
+import com.amaraldev.workshopmongo.repository.PostRepository;
+import com.amaraldev.workshopmongo.services.exception.ObjectNotFoundExeception;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundExeception("Objeto não encontrado"));
+    }
+
+}
