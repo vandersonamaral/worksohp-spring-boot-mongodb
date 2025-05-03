9e60d5e3d4d687c3a741eaae0e929a039ead91a5
vandersonamaral <vandersonamaral27@gmail.com>
2025-05-03 13:13:17 -0300

    feat: Simple query with query methods

--- src/main/java/com/amaraldev/workshopmongo/resources/PostResource.java
+++ src/main/java/com/amaraldev/workshopmongo/resources/PostResource.java
@@ -1,10 +1,14 @@
 package com.amaraldev.workshopmongo.resources;
 
 import com.amaraldev.workshopmongo.domain.Post;
+import com.amaraldev.workshopmongo.resources.util.URL;
 import com.amaraldev.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 
 @RestController
@@ -18,6 +22,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String title){
+        title= URL.decodeParam(title);
+        List<Post> list = postService.findByTitle(title);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/amaraldev/workshopmongo/services/PostService.java
+++ src/main/java/com/amaraldev/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.amaraldev.workshopmongo.services.exception.ObjectNotFoundExeception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = postRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundExeception("Objeto não encontrado"));
+    }
+    public List<Post> findByTitle(String title){
+        return postRepository.findByTitleContainingIgnoreCase(title);
     }
 
 }
